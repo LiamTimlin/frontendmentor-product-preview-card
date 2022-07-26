71b938759b015eb966f5ec51164dad1b868a6e2c
LiamTimlin <williamtimlin@outlook.com>
2022-07-26 10:31:28 +0200

    reset styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-    return <main className="bg-green-300 min-h-screen"></main>;
+    return <main></main>;
 };
 
 export default App;
